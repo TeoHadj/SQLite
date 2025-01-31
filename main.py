1a3f160e2d5bca6b156fda6d61d0188f9562551e
TeoHadj <teohad1112@highgateschool.org.uk>
2025-01-31 10:30:43 +0000

    updated drop table + print table

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ connection = sqlite3.connect('space.db')
 
 cursor = connection.cursor()
 
+start = '''
+DROP TABLE IF EXISTS planets;
+'''
+cursor.execute(start)
+
 create_table_planets = '''
 
 CREATE TABLE IF NOT EXISTS planets(
@@ -30,6 +35,13 @@ INSERT INTO planets(name, diameter, galaxy, moons, aliens)
 VALUES(?,?,?,?,?)
 '''
 cursor.execute(insert_data, ("Venus", 12104, "Milky Way", 0, 0))
+
+my_query = '''
+SELECT * FROM PLANETS
+'''
+
+for item in cursor.execute(my_query):
+    print(item)
 
 connection.commit()
 connection.close()
