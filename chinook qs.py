7fe1530ea9e79936bd85bc917511f60b709d20fa
TeoHadj <teohad1112@highgateschool.org.uk>
2025-02-23 20:49:24 +0000

    Added 4 queries

--- chinook qs.py
+++ chinook qs.py
@@ -3,16 +3,67 @@ import sqlite3 as sqlite
 conn = sqlite.connect('db.sqlite')
 
 c = conn.cursor()
+# customer_query = """
+# SELECT Artist.Name, COUNT(Album.AlbumId)
+# FROM Artist
+# INNER JOIN Album ON Artist.ArtistId = Album.ArtistId
+# GROUP BY Artist.ArtistId
+# ORDER BY COUNT(Album.AlbumId) DESC;
+# """
+#
+# c.execute(customer_query)
+#
+# query_response = c.fetchall()
+#
+# for line in query_response:
+#     print(line)
+
+
+
+# customer_query = """
+# SELECT Name, Composer
+# FROM Track
+# WHERE LOWER(Name) LIKE "% love %"
+# OR LOWER(Name) LIKE "love %"
+# OR LOWER(Name) LIKE "% love"
+# OR LOWER(Name) LIKE "love"
+# """
+#
+# c.execute(customer_query)
+#
+# query_response = c.fetchall()
+#
+# for line in query_response:
+#     print(line)
+
+
+# customer_query = """
+# SELECT Track.Name, Composer
+# FROM Track
+# INNER JOIN Genre ON Track.GenreId = Genre.GenreId
+# WHERE Genre.Name = "Rock"
+# """
+#
+# c.execute(customer_query)
+#
+# query_response = c.fetchall()
+#
+# for line in query_response:
+#     print(line)
+
 
 customer_query = """ 
-SELECT COUNT(TrackId)
-FROM Track
+SELECT Album.Title, COUNT(Track.Name) as TrackNo
+FROM Album
+INNER JOIN Track
+ON Album.AlbumId = Track.AlbumId
+GROUP BY Album.AlbumId
+ORDER BY TrackNo DESC
 """
 
 c.execute(customer_query)
 
 query_response = c.fetchone()
 
-print(query_response[0])
-
-
+for line in query_response:
+    print(line)
